9cb01b3282dd78356cf8494a7305dec777bbd86e
Essam-1978 <77950781+Essam-1978@users.noreply.github.com>
2024-10-19 22:17:05 +0300

    Add files via upload

---
+++ js/script3.js
@@ -0,0 +1,58 @@
+
+const burger=document.getElementsByClassName("toggle-button")[0];
+//burger.addEventListener("click", myfunction());
+
+//console.log(burger);
+//prompt('eeeeee');
+//menu.classList.toggle('active');
+ 
+ function myfunction3(){
+    const menu=document.getElementsByClassName("desc1")[0];
+ menu.classList.toggle("active3");
+ }
+
+
+
+
+
+
+
+ function myfunction(){
+    const menu1=document.getElementsByClassName("menu")[0];
+ menu1.classList.toggle("active4");
+ }
+
+
+
+
+
+ function showstaff(){
+    console.log('ggggg');
+    const divstaff=document.getElementsByClassName("agenthide");
+    //divstaff.classList.toggle("agent");
+    //divstaff.setAttribute('class','agent');
+    
+ }
+
+//  const X=document.getElementsByClassName("toggletest1")[0];
+//  X.addEventListener("click",myfunction2());
+
+//  function myfunction2(){
+//      X.classList.toggle("toggletest2");
+//     console.log(X);
+//  }
+
+// })
+
+// function toggle_menu_2()
+// {
+// const toggle_button2=document.getElementsByClassName("toggle-button2")[0];
+// const menu2=document.getElementsByClassName("menu2")[0];
+
+// toggle_button2.addEventListener("click",function3());
+
+// function function3(){
+    
+//     menu2.classList.toggle("active2");
+// }
+// }
