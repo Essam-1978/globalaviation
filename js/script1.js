3788bb70afe31acb2cd402d2a43bc4dfec822003
Essam-1978 <77950781+Essam-1978@users.noreply.github.com>
2024-10-19 22:09:33 +0300

    Delete js directory

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-
-const burger=document.getElementsByClassName("arrow")[0];
-
- 
-function myfunction(){
-    const menu1=document.getElementsByClassName("menu")[0];
- menu1.classList.toggle("active4");
- }
-
